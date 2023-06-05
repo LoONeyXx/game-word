922dba21482a0c234465f297881e3d17b79e7444
Sergey Gromov <looneyxx@yandex.ru>
2023-06-05 08:48:03 +0300

    Working version

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
 import './App.css';
 import Letter from './Letter';
 import React from 'react';
+import contextCapitals from '../contexts/Capitals.jsx';
+
 function App() {
-    const [word, setWord] = React.useState('ТЕСТОВЫЙ');
+    const capitals = React.useContext(contextCapitals);
+    const [word, setWord] = React.useState('');
     const [isPlaying, setPlay] = React.useState(true);
     const [popupText, setPpopupText] = React.useState('');
     const [lettersWord, setLettersWord] = React.useState({});
+    const [question, setQuestion] = React.useState('');
+
+
+    const onSubmit = React.useCallback(() => {
+        const newCapital = capitals[randomIntFromInterval(1, 247)];
+        setPlay(true);
+        setWord('');
+        setTimeout(() => {
+            setQuestion(newCapital.country);
+            setWord(newCapital.capital.split(' ').join('').toUpperCase());
+        }, 0);
+    },[]);
+
+    const handleKey = React.useCallback(
+        (e) => {
+            const key = e.key.toUpperCase();
+            if (e.key === 'Enter' && !isPlaying) {
+                onSubmit();
+            }
+            if (word.indexOf(key) > -1 && lettersWord[key] > 0) {
+                setLettersWord((prev) => ({ ...prev, [key]: 0 }));
+            }
+        },
+        [lettersWord, word, isPlaying, onSubmit]
+    );
+    function randomIntFromInterval(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    React.useEffect(() => {
+        onSubmit();
+    }, [onSubmit]);
 
     React.useEffect(() => {
         setLettersWord(
@@ -29,23 +64,6 @@ function App() {
         }
     }, [lettersWord]);
 
-    function onSubmit() {
-        setPlay(true);
-        setWord('')
-        setTimeout(() => {
-            setWord('ТЕСТОВЫЙ');
-        }, 0);
-    }
-    const handleKey = React.useCallback(
-        (e) => {
-            const key = e.key.toUpperCase();
-            if (word.indexOf(key) > -1 && lettersWord[key] > 0) {
-                setLettersWord((prev) => ({ ...prev, [key]: 0 }));
-            }
-        },
-        [lettersWord, word]
-    );
-
     React.useEffect(() => {
         window.addEventListener('keydown', handleKey);
         return () => {
@@ -55,10 +73,11 @@ function App() {
     return (
         <div className='App'>
             <header className='header'>
-                <h1 className='header__title'>Game Word</h1>
+                <h1 className='header__title'>Check your skill</h1>
             </header>
             <main className='content'>
                 <section className='desk'>
+                    <h2 className='desk__question'>{question}</h2>
                     <ul className='desk__container'>
                         {word.split('').map((letter, index) => (
                             <Letter key={index} isVisible={lettersWord[letter] === 0} letter={letter} />
---
+++ src/contexts/Capitals.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import capitals from './capitals.json'
+
+
+
+const contextCapitals = React.createContext(capitals)
+
+export default contextCapitals
